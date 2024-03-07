ba1a8e956264cc38fb27c66eb9d29f03d2cc9d24
taranvd <apple.vlad.taran@gmail.com>
2024-03-07 21:30:38 +0200

    "feat: Added storage of timestamps in UTC timezone for 'users' and 'contacts' tables"

--- nodejs/db_changes.sql
+++ nodejs/db_changes.sql
@@ -1,21 +1,23 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT unique_user_id UNIQUE (id)
+    email VARCHAR(100) NOT NULL,
+    password_hash VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP AT TIME ZONE 'UTC',
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP AT TIME ZONE 'UTC',
+    CONSTRAINT unique_user_id UNIQUE (id),
+    INDEX idx_email (email) 
 );
-
 
 CREATE TABLE contacts (
     contact_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    phone_number VARCHAR(255) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(10) NOT NULL,
     user_id INTEGER NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP AT TIME ZONE 'UTC',
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP AT TIME ZONE 'UTC',
     CONSTRAINT unique_contact_id UNIQUE (contact_id),
-    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id)
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id),
+    INDEX idx_user_id (user_id), 
+    INDEX idx_phone_number (phone_number) 
 );
