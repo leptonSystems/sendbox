a88cbc0bc773b0970774f3f9feb5760ce3952c3c
taranvd <apple.vlad.taran@gmail.com>
2024-03-07 22:00:02 +0200

    refactor: Reduce 'phone_number' length to 22 characters in 'contacts' table

--- nodejs/db_changes.sql
+++ nodejs/db_changes.sql
@@ -12,7 +12,7 @@ CREATE TABLE contacts (
     contact_id SERIAL PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
-    phone_number VARCHAR(10) NOT NULL,
+    phone_number VARCHAR(22) NOT NULL,
     user_id INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP AT TIME ZONE 'UTC',
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP AT TIME ZONE 'UTC',
