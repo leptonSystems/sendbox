46ab745efe61e8ead5d41739bd431561c2fba784
taranvd <apple.vlad.taran@gmail.com>
2024-03-07 19:02:47 +0200

    feat: added SQL queries for creating database

---
+++ nodejs/db_changes.sql
@@ -0,0 +1,19 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    CONSTRAINT unique_user_id UNIQUE (id)
+);
+
+
+CREATE TABLE contacts (
+    contact_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT unique_contact_id UNIQUE (contact_id),
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id)
+);
