beff3b78cad37cb17634f627eb7424b6fcb5b703
taranvd <apple.vlad.taran@gmail.com>
2024-03-08 17:57:10 +0200

    refactor: Removed the excess and left only the basic structure

--- nodejs/src/app.module.ts
+++ nodejs/src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { SequelizeModule } from '@nestjs/sequelize';
 import { ConfigModule } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ContactsModule } from './contacts/contacts.module';
 
 @Module({
   controllers: [AppController],
@@ -15,19 +11,6 @@ import { ContactsModule } from './contacts/contacts.module';
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      models: [],
-      autoLoadModels: true,
-    }),
-    AuthModule,
-    UsersModule,
-    ContactsModule,
   ],
 })
 export class AppModule {}
